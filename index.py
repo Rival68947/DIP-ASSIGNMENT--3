7e148416f0563f1782ad18a8a1d8c3bb144e63ef
Rival68947 <147317122+Rival68947@users.noreply.github.com>
2026-04-13 23:49:33 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 """
-Name: Lakshay
-Roll No: 2301010306
+Name: Manish Pandey
+Roll No: 2301010302
 Course: Image Processing & Computer Vision
 Unit: Mini Project
 Assignment Title: Compression and Segmentation of Medical Images using Python
